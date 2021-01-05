e3c98361b0ffe45bc5986af6300066306477aee7
Dave Defourneaux <dave.defourneaux@gmail.com>
2021-01-05 14:34:34 -0500

    Add Project: Alert and Expressions
    Alert and Expressions.  Basic HTML file links to main.js to demonstrate alert boxes, variable assignments, strings, concatenation, expressions, and commenting of code.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+window.alert("Hello, World!");  // Display a string in an alert window
+
+document.write('Hello, World!!  ');  // Print a string to the screen
+
+var A = "This is a string.  ";  // Assign a string to a variable
+document.write(A);  // Print the value of the variable to the screen
+
+var x = "Dave Defourneaux is (almost)";  // Assign a string to a variable
+window.alert(x);  // Display the value in an alert window
+
+var y = "A" + ' world of ' + 'hurt';  // Assign a concatenated string to a variable 
+document.write(y);  // Print the value to the screen
+
+document.write("...so I sez, \"Hey! Aren\'t ya gonna eat yer fat?\"   ");  // Utilize escape characters
+
+document.write("Spaulding, " + "get your foot off the boat!   ");  // Print a concatenated string to the screen
+
+var x = "Dave ", y = "Baxter ", z = "Izzy ";  // Assign multiple variables on one line
+var x = x.fontcolor("red");  // Change font color of variable to red
+var y = y.fontcolor("blue");  // Change font color of variable to blue
+var z = z.fontcolor("green");  // Change font color of variable to green
+document.write(x);  // Print value of variable to screen
+document.write(y);  // Print value of variable to screen
+document.write(z);  // Print value of variable to screen
+
+var B = 2021 - 1968;  // Assign an expression to a variable
+window.alert(B);  // Display the value in an alert window
+
+// Asign string values to variables and concatenate the variables
+var C = "     \"What the world needs now ", D = " is another folk singer, ", E = "like I need a hole in my head.\"";
+document.write(C,D,E);
